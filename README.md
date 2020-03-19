1f78b0836a29e3fcf0d9c6469f1287f8134bd8f3
Shirasawa <764798966@qq.com>
2020-03-19 17:57:11 +0800

    Init: first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# NekoCraftOfficialWebsite ![Deploy](https://github.com/neko-craft/NekoCraftOfficialWebsite/workflows/Deploy/badge.svg)
+
+## Build
+
+```bash
+npm install
+
+npm run build
+```
+
+## Author
+
+Shirasawa
+
+## License
+
+[MIT](./LICENSE)
---
+++ index.js
@@ -0,0 +1,29 @@
+import Swiper from 'swiper'
+const bgs = require('./assets/backgrounds/*.jpg')
+const images = require('./assets/images/*.jpg')
+console.log(images)
+
+const keys = Object.keys(bgs)
+document.getElementById('first').style.backgroundImage = `url(${bgs[keys[Math.random() * keys.length | 0]]})`
+const slide = document.getElementById('slide')
+Object.keys(images).sort(() => Math.random() - 0.5).forEach(it => {
+  const img = new Image()
+  img.className = 'swiper-slide'
+  img.src = images[it]
+  slide.appendChild(img)
+})
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
